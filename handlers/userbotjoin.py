c924c9587480fe991b30d073f15a0984d76f4330
GAMERFUCKER <bsdkbhaag248@gmail.com>
2021-04-16 19:15:18 +0530

    Create userbotjoin.py

---
+++ handlers/userbotjoin.py
@@ -0,0 +1,41 @@
+from tgcalls.tgcalls import client as USER
+from pyrogram import Client, filters
+from pyrogram.types import Message, InlineKeyboardButton, InlineKeyboardMarkup
+from pyrogram.errors import UserAlreadyParticipant
+from helpers.decorators import errors, authorized_users_only
+
+@Client.on_message(filters.group & filters.command(["userbotjoin"]))
+@authorized_users_only
+@errors
+async def addchannel(client, message):
+    chid = message.chat.id
+    try:
+        invitelink = await client.export_chat_invite_link(chid)
+    except:
+        await message.reply_text(
+            "<b>Add me as admin of yor group first</b>",
+        )
+        return
+
+    try:
+        user = await USER.get_me()
+    except:
+        user.first_name =  "cuXmusic"
+
+    try:
+        await USER.join_chat(invitelink)
+    except UserAlreadyParticipant:
+        await message.reply_text(
+            "<b>@ already in your chat</b>",
+        )
+        pass
+    except Exception as e:
+        print(e)
+        await message.reply_text(
+            f"<b>User {user.first_name} couldn't join your group! Make sure user is not banned in group."
+            "\n\nOr manually add @ to your Group and try again</b>",
+        )
+        return
+    await message.reply_text(
+            "<b>@ userbot joined your chat</b>",
+        )
