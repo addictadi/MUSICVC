2365c6f59ebe96886839b2d0e1b647b0e839ac9c
GAMERFUCKER <bsdkbhaag248@gmail.com>
2021-04-16 20:01:05 +0530

    Update userbotjoin.py

--- handlers/userbotjoin.py
+++ handlers/userbotjoin.py
@@ -20,7 +20,7 @@ async def addchannel(client, message):
     try:
         user = await USER.get_me()
     except:
-        user.first_name =  "cuXmusic"
+        user.first_name =  "cheemsmusic"
 
     try:
         await USER.join_chat(invitelink)
